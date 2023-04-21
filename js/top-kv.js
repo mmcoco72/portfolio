766003f4af57bc6a6e227cf65e3280d4b8f61c7e
emma <bad.ema.1011@gmail.com>
2023-04-21 15:34:45 +0900

    edit header

--- js/top-kv.js
+++ js/top-kv.js
@@ -22,15 +22,20 @@ window.onload = function() {
     //ヘッダーの背景ぼかし
     window.addEventListener('scroll', () => {
         const header = document.querySelector(".l-header__inner-wrapper");
+        let blurFlag = false;
         
         window.addEventListener('scroll', () => {
             //スクロール位置取得
             const scrollY = window.pageYOffset;
 
             if (scrollY > 80) {
-                header.style.backdropFilter = 'blur(2px)';
+                // header.style.backdropFilter = 'blur(2px)';
+                header.classList.add('is-blur');
+                blurFlag = true;
             } else {
-                header.style.backdropFilter = 'blur(0)';
+                // header.style.backdropFilter = 'blur(0)';
+                header.classList.remove('is-blur');
+                blurFlag = false;
             }
         });
     });
